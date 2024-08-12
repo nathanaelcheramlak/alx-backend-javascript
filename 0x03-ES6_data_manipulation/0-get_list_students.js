43f8af5330970e12a52f5e59e40ea1f16a6f75af
Nathanael Cheramlak <nathanaelcheramlak7@gmail.com>
2024-08-12 20:30:21 +0300

    data manipulation

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,18 @@
+export default function getListStudents() {
+  const person1 = {
+    id: 1,
+    firstName: "Guillaume",
+    location: "San Francisco",
+  };
+  const person2 = {
+    id: 2,
+    firstName: "James",
+    location: "Columbia",
+  };
+  const person3 = {
+    id: 5,
+    firstName: "Serena",
+    location: "San Francisco",
+  };
+  return [person1, person2, person3];
+}
